fce0ed33a6cdb31fe1a44750fde14c9b0cf653a6
myrzataev <myrzataevmurat20@gmail.com>
2023-11-28 20:02:19 +0600

    735. Asteroid Collision

--- asteroidCollision.dart
+++ asteroidCollision.dart
@@ -1,34 +1,37 @@
 class Solution {
   List<int> asteroidCollision(List<int> asteroids) {
-    int index = 1;
-    int size = 0;
+    int index = 0;
+    int size = 2;
 
-    while (size <= asteroids.length - 1) {
-      if (asteroids[index] > 0 && asteroids[index - 1] > 0) {
-        // result.add(asteroids[index]);
-        index += 1;
-        size += 1;
-      } else if (asteroids[index] < 0 && asteroids[index - 1] < 0) {
-        // result.add(asteroids[index]);
-        index += 1;
-        size += 1;
+    while (size <= asteroids.length) {
+      if (size <= 0 || size == 1) {
+        return asteroids;
       } else {
-        if (asteroids[index].abs() > asteroids[index - 1].abs()) {
-          asteroids.removeAt(index - 1);
-          index -= 1;
-          size -= 1;
-        } else if (asteroids[index].abs() < asteroids[index + 1].abs()) {
-          asteroids.removeAt(index);
-          index -= 1;
-          size -= 1;
+        if (asteroids[index] > 0 && asteroids[index + 1] > 0) {
+          index += 1;
+          size += 1;
+        } else if (asteroids[index] < 0 && asteroids[index + 1] < 0) {
+          index += 1;
+          size += 1;
         } else {
-          asteroids.removeAt(index);
-          size -= 1;
-          asteroids.removeAt(index);
-          size -= 1;
+          if (asteroids[index].abs() > asteroids[index + 1].abs()) {
+            asteroids.removeAt(index + 1);
+            index -= 1;
+            size -= 1;
+          } else if (asteroids[index].abs() < asteroids[index + 1].abs()) {
+            asteroids.removeAt(index);
+            index -= 1;
+            size -= 1;
+          } else {
+            asteroids.removeAt(index);
+            size -= 1;
+            asteroids.removeAt(index);
+            size -= 1;
+          }
         }
       }
     }
+
     return asteroids;
   }
 }
@@ -37,5 +40,5 @@ void main() {
   Solution solution = Solution();
   print(solution.asteroidCollision([5, 10, -5]));
   print(solution.asteroidCollision([8, -8]));
-  print(solution.asteroidCollision([10, 2, -5]));
+  print(solution.asteroidCollision([-2,-1,1,2]));
 }
